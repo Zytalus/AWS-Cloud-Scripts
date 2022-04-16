8a827cdfdd1cfcc490319405ce4f798d034351bd
Nicholas Deckard <nicholas.deckard@crazymagic.studio>
2022-04-16 18:02:43 -0500

    Update PerforceAWSLinux.sh
    enabled ssl communication

--- PerforceAWSLinux.sh
+++ PerforceAWSLinux.sh
@@ -6,6 +6,5 @@ mount /dev/sdb /mnt/depot
 export p4root="/mnt/depot"
 export pwd=$(curl --silent http://169.254.169.254/latest/meta-data/instance-id)
 export localip=$(curl --silent curl http://169.254.169.254/latest/meta-data/local-ipv4)
-export p4port=":1666"
-export p4address=$localip$p4port
+export p4address="ssl:"$localip":1666"
 /opt/perforce/sbin/configure-helix-p4d.sh master -p $p4address -r $p4root -u super -P $pwd --case 0 -n
